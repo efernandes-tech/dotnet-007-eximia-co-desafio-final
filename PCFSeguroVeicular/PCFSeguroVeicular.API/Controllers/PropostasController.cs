b17fcceaf23b2822e1f398eae0bfe0d0ac7d4b07
Éderson Fernandes <efernandes.tech@gmail.com>
2024-11-12 09:24:34 -0300

    save

--- PCFSeguroVeicular/PCFSeguroVeicular.API/Controllers/PropostasController.cs
+++ PCFSeguroVeicular/PCFSeguroVeicular.API/Controllers/PropostasController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace PCFSeguroVeicular.API.Controllers;
 
@@ -7,8 +11,37 @@ namespace PCFSeguroVeicular.API.Controllers;
 [ApiVersion("1.0")]
 public class PropostasController : ControllerBase
 {
+    public record Residencia(
+        string UF,
+        string Cidade,
+        string Bairro);
+
+    public record Cobertura(
+        string Codigo);
+
+    public record NovaPropostaRequest(
+        //Veículo: marca, modelo e ano
+        string veiculoMarca,
+        string veiculoModelo,
+        string veiculoAno,
+        //Proprietário: cpf, nome, data nascimento, residência
+        string proprietarioCpf,
+        string proprietarioNome,
+        string proprietarioDtNascimento,
+        Residencia proprietarioResidencia,
+        //Condutor: cpf, data nascimento, residência.
+        string condutorCpf,
+        string condutorDtNascimento,
+        Residencia condutorResidencia,
+        //Coberturas: coberturas(roubo/furto, colisão, terceiros, proteção residencial) selecionadas pelo usuário
+        Cobertura[] coberturas
+        );
+
     [HttpPost]
-    public async Task<IActionResult> CriarProposta()
+    public async Task<IActionResult> CriarProposta(
+        [FromBody] NovaPropostaRequest input,
+        CancellationToken cancellationToken
+        )
     {
         return Ok();
     }
